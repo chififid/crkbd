b1e5bb30548ce6b210e0de3dbbdea9b4b496a8fd
chififid <57342377+chififid@users.noreply.github.com>
2023-06-13 12:23:14 +0300

    Update work with memory

--- crkbd.c
+++ crkbd.c
@@ -1,3 +1,21 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "quantum.h"
 #include QMK_KEYBOARD_H
 #include <stdio.h>
@@ -24,9 +42,10 @@ __attribute__((weak)) const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRI
 
 #ifdef OLED_ENABLE
 
-#define PI 3.14159
+#define PI 3.1415
 #define WIDTH 128
 #define HEIGHT 32
+#define TRIS_COUNT 12
 
 #define CREATE_VEC3_OPERATION_FUNCTION(FUNNAME, OP) \
   static void FUNNAME(vec3d *res, vec3d *x, vec3d *y) { \
@@ -34,11 +53,6 @@ __attribute__((weak)) const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRI
     res->y = x->y OP y->y; \
     res->z = x->z OP y->z; \
   }
-
-typedef struct vec2d {
-  double x;
-  double y;
-} vec2d;
 
 typedef struct vec3d {
   double x;
@@ -48,97 +62,96 @@ typedef struct vec3d {
 
 typedef vec3d triangle3d[3];
 
-typedef vec2d triangle2d[3];
-
-typedef struct mesh3d {
-  triangle3d *tris;
-  int tris_count;
-} mesh3d;
-
 typedef double mat4x4[4][4];
 
-static const double obj_points[12][9] = {
-    // SOUTH
-		{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
-		{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+typedef struct mesh {
+  vec3d translation;
+  mat4x4 rotation;
+} mesh;
 
-		// EAST                                                      
-		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
-
-		// NORTH                                                     
-		{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
-
-		// WEST                                                      
-		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
-		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
-
-		// TOP                                                       
-		{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
-		{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
-
-		// BOTTOM                                                    
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+static const float PROGMEM obj_points[TRIS_COUNT][9] = {
+  { -1.00000, 1.00000, 1.00000,  -1.00000, -1.00000, -1.00000,  -1.00000, -1.00000, 1.00000 },
+  { -1.00000, -1.00000, -1.00000,  1.00000, 1.00000, -1.00000,  1.00000, -1.00000, -1.00000 },
+  { 1.00000, 1.00000, -1.00000,  1.00000, -1.00000, 1.00000,  1.00000, -1.00000, -1.00000 },
+  { 1.00000, 1.00000, 1.00000,  -1.00000, -1.00000, 1.00000,  1.00000, -1.00000, 1.00000 },
+  { 1.00000, -1.00000, -1.00000,  -1.00000, -1.00000, 1.00000,  -1.00000, -1.00000, -1.00000 },
+  { 1.00000, 1.00000, -1.00000,  -1.00000, 1.00000, 1.00000,  1.00000, 1.00000, 1.00000 },
+  { -1.00000, 1.00000, 1.00000,  -1.00000, 1.00000, -1.00000,  -1.00000, -1.00000, -1.00000 },
+  { -1.00000, -1.00000, -1.00000,  -1.00000, 1.00000, -1.00000,  1.00000, 1.00000, -1.00000 },
+  { 1.00000, 1.00000, -1.00000,  1.00000, 1.00000, 1.00000,  1.00000, -1.00000, 1.00000 },
+  { 1.00000, 1.00000, 1.00000,  -1.00000, 1.00000, 1.00000,  -1.00000, -1.00000, 1.00000},
+  { 1.00000, -1.00000, -1.00000,  1.00000, -1.00000, 1.00000,  -1.00000, -1.00000, 1.00000 },
+  { 1.00000, 1.00000, -1.00000,  -1.00000, 1.00000, -1.00000,  -1.00000, 1.00000, 1.00000 },
 };
 
+// BASIC OPERATIONS
 
 CREATE_VEC3_OPERATION_FUNCTION(sub_vec, -)
 
-static void vec3d_to_vec2d(vec2d *res, vec3d *point) {
-  res->x = point->x;
-  res->y = point->y;
+static void create_vector(vec3d *p, double x, double y, double z) {
+  p->x = x;
+  p->y = y;
+  p->z = z;
 }
 
-static void triangle_to_2d(triangle2d *res, triangle3d *tri) {
+static void translate_vector(vec3d *p, double x, double y, double z) {
+  p->x = p->x + x;
+  p->y = p->y + y;
+  p->z = p->z + z;
+}
+
+static void translate_triangle(triangle3d *triangle, double x, double y, double z) {
   for (int point_i = 0; point_i < 3; point_i++) {
-    vec3d_to_vec2d(&((*res)[point_i]), &((*tri)[point_i]));
+    translate_vector(&((*triangle)[point_i]), x, y, z);
   }
 }
 
-static void load_mesh_from_obj(mesh3d *res) {
-  int tris_count = sizeof(obj_points) / sizeof(obj_points[0]);
+static double dot_vec(vec3d *x, vec3d *y) {
+  return x->x * y->x + x->y * y->y + x->z * y->z;
+}
 
-  res->tris_count = tris_count;
-  res->tris = malloc(sizeof(triangle3d) * tris_count);
+static double vec_length(vec3d *x) {
+  return sqrt(dot_vec(x, x));
+}
 
-  for (int tri_index = 0; tri_index < tris_count; tri_index++) {
-    for (int point_i = 0; point_i < 3; point_i++) {
-      res->tris[tri_index][point_i].x = obj_points[tri_index][3 * point_i + 0];
-      res->tris[tri_index][point_i].y = obj_points[tri_index][3 * point_i + 1];
-      res->tris[tri_index][point_i].z = obj_points[tri_index][3 * point_i + 2];
-    }
+static void multiply_matrix_vector(vec3d *res, vec3d *p, mat4x4 *m) {
+  vec3d temp;
+
+  temp.x = p->x * (*m)[0][0] + p->y * (*m)[1][0] + p->z * (*m)[2][0] + (*m)[3][0];
+  temp.y = p->x * (*m)[0][1] + p->y * (*m)[1][1] + p->z * (*m)[2][1] + (*m)[3][1];
+  temp.z = p->x * (*m)[0][2] + p->y * (*m)[1][2] + p->z * (*m)[2][2] + (*m)[3][2];
+  double w = p->x * (*m)[0][3] + p->y * (*m)[1][3] + p->z * (*m)[2][3] + (*m)[3][3];
+
+  if (w != 0.0) {
+    temp.x /= w;
+    temp.y /= w;
+    temp.z /= w;
+  }
+
+  res->x = temp.x;
+  res->y = temp.y;
+  res->z = temp.z;
+}
+
+static void multiply_matrix_triangle(triangle3d *res, triangle3d *triangle, mat4x4 *m) {
+  for (int point_i = 0; point_i < 3; point_i++) {
+    multiply_matrix_vector(&((*res)[point_i]), &((*triangle)[point_i]), m);
   }
 }
 
-static void oled_write_line(vec2d *start_p, vec2d *end_p, bool on) {
-  int sx = (int)start_p->x;
-  int sy = (int)start_p->y;
-  int ex = (int)end_p->x;
-  int ey = (int)end_p->y;
-  int x_sub = ex - sx;
-  int y_sub = ey - sy;
-  int x_dist = fabs(x_sub);
-  int y_dist = fabs(y_sub);
-  double ratio = (double)y_sub / (double)x_sub;
+// RENDER CODE
 
-  if (x_dist > y_dist) {
-    for (int dx = 0; 1; dx += (int)(x_sub / x_dist)) {
-      oled_write_pixel((uint8_t)(sx + dx), (uint8_t)(sy + ratio * dx), on);
-      
-      if (dx == x_sub) {
-        break;
-      }
-    }
-  } else {
-    for (int dy = 0; 1; dy += (int)(y_sub / y_dist)) {
-      oled_write_pixel((uint8_t)(sx + (1.0 / ratio) * dy), (uint8_t)(sy + dy), on);
-
-      if (dy == y_sub) {
-        break;
-      }
-    }
+static void load_triangle_from_obj(triangle3d *res, int tri_index) {
+  for (int point_i = 0; point_i < 3; point_i++) {
+    (*res)[point_i].x = pgm_read_float(
+      &(obj_points[tri_index][3 * point_i + 0])
+    );
+    (*res)[point_i].y = pgm_read_float(
+      &(obj_points[tri_index][3 * point_i + 1])
+    );
+    (*res)[point_i].z = pgm_read_float(
+      &(obj_points[tri_index][3 * point_i + 2])
+    );
   }
 }
 
@@ -181,62 +194,11 @@ static void create_camera_point(vec3d *point, double x, double y, double z) {
   point->z = z;
 }
 
-static void translate_vector(vec3d *p, vec3d *r, double x, double y, double z) {
-  r->x = p->x + x;
-  r->y = p->y + y;
-  r->z = p->z + z;
-}
-
-static void translate_mesh(mesh3d *obj, double x, double y, double z) {
-  for (int tri_index = 0; tri_index < obj->tris_count; tri_index++) {
-    for (int point_i = 0; point_i < 3; point_i++) {
-      translate_vector(&(obj->tris[tri_index][point_i]), &(obj->tris[tri_index][point_i]), x, y, z);
-    }
-  }
-}
-
-static void multiply_matrix_vector(vec3d *i, vec3d *o,  mat4x4 *m) {
-  double w;
-
-  o->x = i->x * (*m)[0][0] + i->y * (*m)[1][0] + i->z * (*m)[2][0] + (*m)[3][0];
-  o->y = i->x * (*m)[0][1] + i->y * (*m)[1][1] + i->z * (*m)[2][1] + (*m)[3][1];
-  o->z = i->x * (*m)[0][2] + i->y * (*m)[1][2] + i->z * (*m)[2][2] + (*m)[3][2];
-  w    = i->x * (*m)[0][3] + i->y * (*m)[1][3] + i->z * (*m)[2][3] + (*m)[3][3];
-
-  if (w != 0.0) {
-    o->x /= w;
-    o->y /= w;
-    o->z /= w;
-  }
-}
-
-static void multiply_mesh(mesh3d *obj, mat4x4 *m) {
-   for (int tri_index = 0; tri_index < obj->tris_count; tri_index++) {
-    for (int point_i = 0; point_i < 3; point_i++) {
-      vec3d new_p;
-      
-      multiply_matrix_vector(&(obj->tris[tri_index][point_i]), &new_p, m);
-
-      obj->tris[tri_index][point_i].x = new_p.x;
-      obj->tris[tri_index][point_i].y = new_p.y;
-      obj->tris[tri_index][point_i].z = new_p.z;
-    }
-  }
-}
-
 static void normalize_to_screen(vec3d *p) {
   p->x += 1.0;
   p->y += 1.0;
   p->x *= 0.5 * (double)WIDTH;
   p->y *= 0.5 * (double)HEIGHT;
-}
-
-double dot_vec(vec3d *x, vec3d *y) {
-  return x->x * y->x + x->y * y->y + x->z * y->z;
-}
-
-double vec_length(vec3d *x) {
-  return sqrt(dot_vec(x, x));
 }
 
 static void get_normal(vec3d *normal, vec3d *p1, vec3d *p2, vec3d *p3) {
@@ -254,25 +216,67 @@ static void get_normal(vec3d *normal, vec3d *p1, vec3d *p2, vec3d *p3) {
   normal->z /= normal_l;
 }
 
-mesh3d *obj;
+static void oled_write_line(vec3d *start_p, vec3d *end_p, bool on) {
+  int sx = (int)start_p->x;
+  int sy = (int)start_p->y;
+  int ex = (int)end_p->x;
+  int ey = (int)end_p->y;
+  int x_sub = ex - sx;
+  int y_sub = ey - sy;
+  int x_dist = fabs(x_sub);
+  int y_dist = fabs(y_sub);
+  double ratio = (double)y_sub / (double)x_sub;
+
+  if (x_dist > y_dist) {
+    for (int dx = 0; 1; dx += (int)(x_sub / x_dist)) {
+      oled_write_pixel((uint8_t)(sx + dx), (uint8_t)(sy + ratio * dx), on);
+      
+      if (dx == x_sub) {
+        break;
+      }
+    }
+  } else {
+    for (int dy = 0; 1; dy += (int)(y_sub / y_dist)) {
+      oled_write_pixel((uint8_t)(sx + (1.0 / ratio) * dy), (uint8_t)(sy + dy), on);
+
+      if (dy == y_sub) {
+        break;
+      }
+    }
+  }
+}
+
 mat4x4 *projection_m, *rotation_m;
 vec3d *camera_point;
+mesh *obj;
 static void render_cube(void) {
-  translate_mesh(obj, 0.0, 0.0, 1.7);
-
-  for (int tri_index = 0; tri_index < obj->tris_count; tri_index++) {
+  for (int tri_index = 0; tri_index < TRIS_COUNT; tri_index++) {
+    triangle3d triangle;
     vec3d normal, camera_to_point;
+
+    load_triangle_from_obj(&triangle, tri_index);
+
+    multiply_matrix_triangle(&triangle, &triangle, &(obj->rotation));
+
+    translate_triangle(
+      &triangle,
+      obj->translation.x,
+      obj->translation.y,
+      obj->translation.z
+    );
+
+    translate_triangle(&triangle, 0, 0, 3.5);
     
     get_normal(
       &normal,
-      &(obj->tris[tri_index][0]),
-      &(obj->tris[tri_index][1]),
-      &(obj->tris[tri_index][2])
+      &(triangle[0]),
+      &(triangle[1]),
+      &(triangle[2])
     );
 
     sub_vec(
       &camera_to_point,
-      &(obj->tris[tri_index][0]),
+      &(triangle[0]),
       camera_point
     );
 
@@ -280,47 +284,45 @@ static void render_cube(void) {
       continue;
     }
 
-    triangle3d projected_triangle;
     for (int point_i = 0; point_i < 3; point_i++) {
-      multiply_matrix_vector(&(obj->tris[tri_index][point_i]), &(projected_triangle[point_i]), projection_m);
-      normalize_to_screen(&(projected_triangle[point_i]));
+      multiply_matrix_vector(&(triangle[point_i]), &(triangle[point_i]), projection_m);
+      normalize_to_screen(&(triangle[point_i]));
     }
-
-    triangle2d show_triangle;
-    triangle_to_2d(&show_triangle, &projected_triangle);
 
     for (int point_i = 0; point_i < 3; point_i++) {
       int next_point_i = (point_i + 1) % 3;
 
-      oled_write_line(&(show_triangle[point_i]), &(show_triangle[next_point_i]), true);
+      oled_write_line(&(triangle[point_i]), &(triangle[next_point_i]), true);
     }
   }
-
-  translate_mesh(obj, 0.0, 0.0, -1.7);
 }
 
 void keyboard_pre_init_user(void) {
-  obj = malloc(sizeof(mesh3d));
-  load_mesh_from_obj(obj);
+  obj = malloc(sizeof(mesh));
+
+  create_vector(&(obj->translation), 0, 0, 0);
 
   projection_m = malloc(sizeof(mat4x4));
   create_projection_m(projection_m, 0.1, 10.0, 60.0);
 
-  rotation_m = malloc(sizeof(mat4x4));
-  create_rotation_m(rotation_m, PI / 18, PI / 27, PI / 90);
-
   camera_point = malloc(sizeof(vec3d));
   create_camera_point(camera_point, 0, 0, 0);
-
-  translate_mesh(obj, -0.5, -0.5, -0.5);
 }
 
+int tick;
 bool oled_task_user(void) {
   oled_clear();
+  create_rotation_m(
+    &(obj->rotation),
+    (PI / 2),
+    (PI / 54) * tick,
+    (PI / 36) * tick
+  );
+  obj->translation.x = sin(((double)tick / 27) * PI);
   oled_render();
-  multiply_mesh(obj, rotation_m);
   render_cube();
 
+  tick = (tick + 1) % 180;
   return false;
 }
 
